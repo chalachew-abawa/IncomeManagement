3f07a272fbbbc26fd9aa6bc3c96ab40b1f82288c
chalachew-abawa <Chalachew.abawa@gmail.com>
2024-11-17 15:49:39 -0500

    first commit

---
+++ src/main/java/dev/chale/IncomeManagement/IncomeManagementApplication.java
@@ -0,0 +1,13 @@
+package dev.chale.IncomeManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncomeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncomeManagementApplication.class, args);
+	}
+
+}
